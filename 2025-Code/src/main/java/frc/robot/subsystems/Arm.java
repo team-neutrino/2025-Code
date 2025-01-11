a5ec149dfb49b7bafceb2f8c55bcf5225324aced
Dialga-Neutrino <neutrino3928@outlook.com>
2025-01-11 11:07:47 -0600

    Imported code
    Imported rev robotics and other code from 2024

--- 2025-Code/src/main/java/frc/robot/subsystems/Arm.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Arm.java
@@ -4,7 +4,31 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkMaxConfigAccessor;
+
+import edu.wpi.first.wpilibj.Timer;
+import java.util.TreeMap;
+
+import com.revrobotics.AbsoluteEncoder;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.MessageTimers;
+import frc.robot.Constants.MotorIDs;
+import frc.robot.Constants.LEDConstants.States;
 
 public class Arm extends SubsystemBase {
   /** Creates a new Arm. */
