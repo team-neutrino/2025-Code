b18288c9f878565c81e4d4bcfcaab20cf06401f4
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-01-11 11:40:11 -0600

    Working on LED

--- 2025-Code/src/main/java/frc/robot/subsystems/LED.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/LED.java
@@ -6,9 +6,44 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LEDSubsystem extends SubsystemBase {
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpil
+
+ibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LEDConstants;
+
+public class LED extends SubsystemBase {
+  private AddressableLED m_addressableLED;
+  private AddressableLEDBuffer m_LEDBuffer;
+  private Timer timer = new Timer();
+  private int m_port;
+
   /** Creates a new LEDSubsystem. */
-  public LEDSubsystem() {}
+  public LED() {
+  }
+
+  public void LEDSubsystem(int p_port) {
+    m_port = p_port;
+    m_addressableLED = new AddressableLED(m_port);
+    m_LEDBuffer = new AddressableLEDBuffer(LEDConstants.LEDBufferLen);
+    m_addressableLED.setLength(m_LEDBuffer.getLength());
+    m_addressableLED.setData(m_LEDBuffer);
+    m_addressableLED.start();
+    setToOrange();
+    timer.start();
+  }
+
+  private boid seToColor(int r, int g, intb) {
+    for (int i=0; i<m_LEDBuffer.getLength(); i++) {
+      m_LEDBuffer.setRGB(i, r, g, b);
+    }
+  }
+
+  public void setToPink() {
+    setToColor(255, 141, 161);
+  }
 
   @Override
   public void periodic() {
