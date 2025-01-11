f8febbc097add68ffc6dfa2c69aaac8442be9d55
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-01-11 12:26:09 -0600

    Started Network Tables

--- 2025-Code/src/main/java/frc/robot/subsystems/LED.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/LED.java
@@ -5,44 +5,21 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpil
-
-ibj.Timer;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.LEDConstants;
+import edu.wpi.first.networktables.StringTopic;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StringPublisher;
 
 public class LED extends SubsystemBase {
-  private AddressableLED m_addressableLED;
-  private AddressableLEDBuffer m_LEDBuffer;
-  private Timer timer = new Timer();
-  private int m_port;
 
-  /** Creates a new LEDSubsystem. */
+  NetworkTableInstance inst = NetworkTableInstance.getDefault();
+  StringTopic Color_topic = inst.getStringTopic("/LED/color");
+
+  final StringPublisher color_pub;
+
+  // Creates a new LED Subsystem
   public LED() {
-  }
-
-  public void LEDSubsystem(int p_port) {
-    m_port = p_port;
-    m_addressableLED = new AddressableLED(m_port);
-    m_LEDBuffer = new AddressableLEDBuffer(LEDConstants.LEDBufferLen);
-    m_addressableLED.setLength(m_LEDBuffer.getLength());
-    m_addressableLED.setData(m_LEDBuffer);
-    m_addressableLED.start();
-    setToOrange();
-    timer.start();
-  }
-
-  private boid seToColor(int r, int g, intb) {
-    for (int i=0; i<m_LEDBuffer.getLength(); i++) {
-      m_LEDBuffer.setRGB(i, r, g, b);
-    }
-  }
-
-  public void setToPink() {
-    setToColor(255, 141, 161);
+    color_pub = Color_topic.publish();
+    color_pub.setDefault(getName());
   }
 
   @Override
