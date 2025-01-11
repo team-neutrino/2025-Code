ccc766bbf6c1586d7e554f17952526283f070f8c
Dialga-Neutrino <neutrino3928@outlook.com>
2025-01-11 12:27:54 -0600

    Added arm commands and constants
    Added arm default command, move command, and added position constants.

--- 2025-Code/src/main/java/frc/robot/Constants.java
+++ 2025-Code/src/main/java/frc/robot/Constants.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -22,7 +26,14 @@ public final class Constants {
   }
 
   public static class ArmConstants {
-
+    // change positions
+    public static final double DEFAULT_ARM_POSITION = 0.0;
+    public static final double GROUND_ALGAE_ARM_POSITION = 0.0;
+    public static final double CORAL_STATION_ARM_POSITION = 0.0;
+    public static final double L1_ARM_POSITION = 0.0;
+    public static final double L2_ARM_POSITION = 0.0;
+    public static final double L3_ARM_POSITION = 0.0;
+    public static final double L4_ARM_POSITION = 0.0;
   }
 
   public static class ClawConstants {
@@ -39,6 +50,6 @@ public final class Constants {
 
   public static class LEDConstants {
 
-  } 
+  }
 
 }
--- 2025-Code/src/main/java/frc/robot/subsystems/Arm.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Arm.java
@@ -4,39 +4,39 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkBase;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkFlexConfig;
-import com.revrobotics.spark.config.SparkFlexConfigAccessor;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkMaxConfigAccessor;
-
-import edu.wpi.first.wpilibj.Timer;
-import java.util.TreeMap;
-
-import com.revrobotics.AbsoluteEncoder;
-import edu.wpi.first.math.filter.Debouncer;
-import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.AbsoluteEncoder;
 
 public class Arm extends SubsystemBase {
+
   private SparkFlex m_armMotor = new SparkFlex(0, null);
   private SparkFlexConfig m_armMotorConfig = new SparkFlexConfig();
 
   public Arm() {
   }
 
+  public double getArmPosition() {
+    return 0.0;
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public Command ArmDefaultCommand() {
+    return Commands.runOnce(() -> {
+    });
+  }
+
+  public Command ArmMoveCommand(double movementAmount) {
+    return Commands.runOnce(() -> {
+    });
+  }
+
 }
