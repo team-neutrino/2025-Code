d5bb9b9c1139886c4f2062cbdc797089907e3899
Andrew <gurellia53@gmail.com>
2025-01-11 14:50:24 -0600

    create a color sensor and send RGB, IR, and proximity to network tables

--- 2025-Code/src/main/java/frc/robot/subsystems/Elevator.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Elevator.java
@@ -9,6 +9,7 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.ColorSensorV3;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -17,6 +18,9 @@ import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
@@ -32,6 +36,11 @@ public class Elevator extends SubsystemBase {
   private SparkFlexConfig m_followerConfig = new SparkFlexConfig();
 
   private double m_target = 0.0;
+
+  // MXP i2c board. Don't use the RIO onboard because it has lockup issues
+  private final I2C.Port i2cPort = I2C.Port.kMXP;
+  // https://codedocs.revrobotics.com/java/com/revrobotics/colorsensorv3
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
   public Elevator() {
     m_config
@@ -86,6 +95,19 @@ public class Elevator extends SubsystemBase {
   @Override
   public void periodic() {
     adjustElevator(m_target);
+    
+    // color sensor
+    Color detectedColor = m_colorSensor.getColor();
+    double rawValue = m_colorSensor.getIR();
+
+    // 0-2047
+    int proximity = m_colorSensor.getProximity();
+
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("IR", rawValue);
+    SmartDashboard.putNumber("Proximity", proximity);
   }
 
   @Override
