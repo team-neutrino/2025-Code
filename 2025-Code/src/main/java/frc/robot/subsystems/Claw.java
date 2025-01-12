5bf66b8752c23f4a44882653ee53cfa3d1ab8569
Anirudh <115200816+Anirudh4Neutrino@users.noreply.github.com>
2025-01-11 22:41:33 -0600

    Update Claw.java

--- 2025-Code/src/main/java/frc/robot/subsystems/Claw.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Claw.java
@@ -7,14 +7,23 @@ package frc.robot.subsystems;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.servohub.ServoHub.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -33,8 +42,9 @@ public class Claw extends SubsystemBase {
     private RelativeEncoder m_followerEncoder;
 
     private double clawVoltage;
+    private double wristAngle;
     private boolean isBroken;
-    private DigitalInput m_intakeBeamBreak = new DigitalInput(ClawConstants.INTAKE_MOTOR_BEAMBREAK);
+    private DigitalInput m_intakeBeamBreak = new DigitalInput(ClawConstants.INTAKEMOTORBEAMBREAK);
 
     /** Creates a new ExampleSubsystem. */
     public Claw() {
@@ -77,12 +87,12 @@ public class Claw extends SubsystemBase {
             stopIntake();
         } else {
 
-            clawVoltage = ClawConstants.INTAKE_MOTOR_VOLTAGE;
+            clawVoltage = ClawConstants.INTAKEMOTORVOLTAGE;
         }
     }
 
     public void runOuttake() {
-        clawVoltage = -ClawConstants.INTAKE_MOTOR_VOLTAGE;
+        clawVoltage = -ClawConstants.INTAKEMOTORVOLTAGE;
     }
 
     public void stopIntake() {
@@ -95,24 +105,62 @@ public class Claw extends SubsystemBase {
 
     @Override
     public void periodic() {
+        Wrist wrist = new Wrist();
         m_grabber.set(clawVoltage);
         isBroken = !m_intakeBeamBreak.get();
+        wrist.moveToPosition(wristAngle);
     }
 
-    // instance or static class???
-    private static class Wrist {
+    private class Wrist {
         private SparkMax m_wrist = new SparkMax(ClawConstants.WRIST, MotorType.kBrushless);
         private SparkMaxConfig m_wristConfig = new SparkMaxConfig();
-        private AbsoluteEncoder m_wristEncoder;
+        private AbsoluteEncoder m_wristEncoder = m_wrist.getAbsoluteEncoder();
+        private SparkClosedLoopController pidController = m_wrist.getClosedLoopController();
 
-        protected Wrist() {
-            m_wristConfig.smartCurrentLimit(Constants.ClawConstants.WRISTCURRENTLIMIT);
+        private Wrist() {
+
+            m_wristConfig.smartCurrentLimit(ClawConstants.WRISTCURRENTLIMIT);
             m_wristConfig.idleMode(IdleMode.kBrake);
-            m_wristConfig.softLimit.forwardSoftLimitEnabled(false);
+
+            m_wristConfig.softLimit.forwardSoftLimit(ClawConstants.MAXIMUMANGLE);
+            m_wristConfig.softLimit.reverseSoftLimit(ClawConstants.MINIMUMANGLE);
+            m_wristConfig.softLimit.forwardSoftLimitEnabled(true);
             m_wristConfig.softLimit.reverseSoftLimitEnabled(true);
             m_wrist.configure(m_wristConfig, SparkBase.ResetMode.kResetSafeParameters,
                     SparkBase.PersistMode.kPersistParameters);
+            AbsoluteEncoderConfig m_wristEncoderConfig = m_wristConfig.absoluteEncoder;
+            m_wristEncoderConfig.zeroOffset(0.0);
+            m_wristEncoderConfig.positionConversionFactor(360.0);
+            ClosedLoopConfig pidConfig = m_wristConfig.closedLoop;
+            pidConfig.pidf(ClawConstants.KP, ClawConstants.KI, ClawConstants.KD, ClawConstants.KFF);
+            pidConfig.outputRange(-1.0, 1.0);
+            pidConfig.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
+            pidConfig.maxMotion.maxVelocity(ClawConstants.MAXVELOCITY);
+            pidConfig.maxMotion.maxAcceleration(ClawConstants.MAXACCELERATION);
+            pidConfig.maxMotion.allowedClosedLoopError(ClawConstants.ALLOWEDERROR);
+        }
+
+        public void moveToPosition(double angle) {
+            if (angle < ClawConstants.MINIMUMANGLE) {
+                angle = ClawConstants.MINIMUMANGLE;
+            } else if (angle > ClawConstants.MAXIMUMANGLE) {
+                angle = ClawConstants.MAXIMUMANGLE;
+            }
+            pidController.setReference(angle, ControlType.kPosition);
+        }
+
+        public void stopWrist() {
+            m_wrist.stopMotor();
+        }
+
+        public double getCurrentPosition() {
+            return m_wristEncoder.getPosition();
+        }
+
+        public boolean isAtPosition(double angle) {
+            return Math.abs(getCurrentPosition() - angle) <= ClawConstants.ALLOWEDERROR;
         }
 
     }
+
 }
