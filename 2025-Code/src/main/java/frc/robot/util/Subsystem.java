9d00ce7c8996602988024c4faef79cf0414165cf
Neutrino Palkia <neutrino3928@outlook.com>
2025-01-14 16:48:11 -0600

    Construct Limelight in Subsystem

--- 2025-Code/src/main/java/frc/robot/util/Subsystem.java
+++ 2025-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -1,7 +1,10 @@
 package frc.robot.util;
 
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Limelight;
 
 public class Subsystem {
     public static final Elevator elevator = new Elevator();
+    public static final Limelight limelight = new Limelight();
+
 }
