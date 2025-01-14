cf3f4fa10a3062cbfb931bf4de97168b5a0eece8
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-01-14 16:45:21 -0600

    Added LED to Subsystem.java

--- 2025-Code/src/main/java/frc/robot/util/Subsystem.java
+++ 2025-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -1,7 +1,10 @@
 package frc.robot.util;
 
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.LED;
 
 public class Subsystem {
     public static final Elevator elevator = new Elevator();
+    public static final LED LED = new LED();
+
 }
