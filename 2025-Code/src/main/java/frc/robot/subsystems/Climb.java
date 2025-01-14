de473ec2aadf61738429b5e3d0e18e196f3a2772
Adam Zhu <887022zhu@ames.k12.ia.us>
2025-01-14 09:45:49 -0600

    Climb Subsystem updates. Added command

--- 2025-Code/src/main/java/frc/robot/subsystems/Climb.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Climb.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbConstants;
 
