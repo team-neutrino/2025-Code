b67d838484a449d6441953fffdaf6368766c854c
Neutrino Beefmaster <neutrino3928@outlook.com>
2025-01-16 17:44:57 -0600

    very basic stuff
    need to pull request arm subsystem into the container rq

--- 2025-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
+++ 2025-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
@@ -1,5 +1,17 @@
 package frc.robot.command_factories;
 
-public class SuperstructureFactory {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.subsystems.*;
+import frc.robot.util.Subsystem;
 
+public class SuperstructureFactory {
+    private static Elevator elevator = Subsystem.elevator;
+    private static Claw claw = Subsystem.claw;
+    // private static Arm arm = Subsystem.arm;
+
+    public static Command intakeCoral() {
+        Command ret = elevator.moveElevatorCommand(ElevatorConstants.CORAL_INTAKE).alongWith(null);
+        return null;
+    }
 }
