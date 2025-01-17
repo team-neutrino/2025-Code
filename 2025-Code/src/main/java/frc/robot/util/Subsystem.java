26af029365b86625460a161451750b91fbfc65f9
Andrew Gurik <gurellia53@gmail.com>
2025-01-16 22:52:21 -0600

    add subsystem enable flags

--- 2025-Code/src/main/java/frc/robot/util/Subsystem.java
+++ 2025-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -3,10 +3,15 @@ package frc.robot.util;
 import frc.robot.subsystems.*;
 
 public class Subsystem {
-    public static final Claw claw = new Claw();
-    public static final Elevator elevator = new Elevator();
-    // public static final Swerve swerve = new Swerve();
-    public static final Limelight limelight = new Limelight();
-    public static final LED LED = new LED();
-    public static final Arm arm = new Arm();
+    public static final boolean ENABLE_SUPERSTRUCTURE = true;
+    public static final boolean ENABLE_DRIVETRAIN = false;
+    public static final boolean ENABLE_LIMELIGHT = false;
+    public static final boolean ENABLE_LED = ENABLE_SUPERSTRUCTURE && ENABLE_DRIVETRAIN && ENABLE_LIMELIGHT;
+
+    public static final Arm arm = ENABLE_SUPERSTRUCTURE ? new Arm() : null;
+    public static final Claw claw = ENABLE_SUPERSTRUCTURE ? new Claw() : null;
+    public static final Elevator elevator = ENABLE_SUPERSTRUCTURE ? new Elevator() : null;
+    public static final Swerve swerve = ENABLE_DRIVETRAIN ? new Swerve() : null;
+    public static final Limelight limelight = ENABLE_LIMELIGHT ? new Limelight() : null;
+    public static final LED LED = ENABLE_LED ? new LED() : null;
 }
