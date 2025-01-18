69e357cd3778fb18f96b4c9a9094988aff985275
Matt Reed <enfcyco@hotmail.com>
2025-01-18 06:19:50 -0600

    Create ArmTest.java

---
+++ 2025-Code/src/test/java/frc/robot/subsystem/ArmTest.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystem;
+
+import org.junit.jupiter.api.Test;
+import frc.robot.subsystems.Arm;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArmTest {
+
+    @Test
+    void testDefaultAngle() {
+        var armSubsystem = new Arm();
+        var armDefault = armSubsystem.getArmPosition();
+        System.out.println("armDefault " + armDefault);
+
+    }
+}
