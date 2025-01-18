64b98601e8014243be706e0922029f19da353edb
Neutrino Angus <neutrino3928@outlook.com>
2025-01-18 10:55:22 -0600

    Made commands one line

--- 2025-Code/src/main/java/frc/robot/command_factories/ArmFactory.java
+++ 2025-Code/src/main/java/frc/robot/command_factories/ArmFactory.java
@@ -7,41 +7,34 @@ import frc.robot.subsystems.Arm;
 
 public class ArmFactory {
     public static Command moveToL1() {
-        Arm arm = Subsystem.arm;
-        return arm.ArmRotateCommand(ArmConstants.L1_POSITION);
+        return Subsystem.arm.ArmRotateCommand(ArmConstants.L1_POSITION);
     }
 
     public static Command moveToL2() {
-        Arm arm = Subsystem.arm;
-        return arm.ArmRotateCommand(ArmConstants.L2_POSITION);
+        return Subsystem.arm.ArmRotateCommand(ArmConstants.L2_POSITION);
     }
 
     public static Command moveToL3() {
-        Arm arm = Subsystem.arm;
-        return arm.ArmRotateCommand(ArmConstants.L3_POSITION);
+        return Subsystem.arm.ArmRotateCommand(ArmConstants.L3_POSITION);
     }
 
     public static Command moveToL4() {
-        Arm arm = Subsystem.arm;
-        return arm.ArmRotateCommand(ArmConstants.L4_POSITION);
+        return Subsystem.arm.ArmRotateCommand(ArmConstants.L4_POSITION);
     }
 
     public static Command armToIntake() {
-        Arm arm = Subsystem.arm;
-        return arm.ArmRotateCommand(ArmConstants.CORAL_STATION_POSITION);
+        return Subsystem.arm.ArmRotateCommand(ArmConstants.CORAL_STATION_POSITION);
     }
     public static Command armToIntake() {
         return arm.ArmRotateCommand(CORAL_STATION_ARM_POSITION);
     }
 
     public static Command armToDescoreL2() {
-        Arm arm = Subsystem.arm;
-        return arm.ArmRotateCommand(ArmConstants.REEF_L2_DESCORE_POSITION);
+        return Subsystem.arm.ArmRotateCommand(ArmConstants.REEF_L2_DESCORE_POSITION);
     }
 
     public static Command armToDescoreL3() {
-        Arm arm = Subsystem.arm;
-        return arm.ArmRotateCommand(ArmConstants.REEF_L3_DESCORE_POSITION);
+        return Subsystem.arm.ArmRotateCommand(ArmConstants.REEF_L3_DESCORE_POSITION);
     }
 
     public static Command armToScore(double level) {
