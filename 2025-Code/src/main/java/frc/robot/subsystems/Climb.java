0ec7a589d7f00e60454fa3a23678feb1af7a1a5e
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-01-18 12:17:24 -0600

    made talonfx configurations

--- 2025-Code/src/main/java/frc/robot/subsystems/Climb.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Climb.java
@@ -10,29 +10,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbConstants;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.spark.ClosedLoopSlot;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.SparkAbsoluteEncoder;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.LimitSwitchConfig;
-import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
 public class Climb extends SubsystemBase {
   private TalonFX m_climbMotor = new TalonFX(ClimbConstants.CLIMB_MOTOR_ID);
   private TalonFX m_followMotor = new TalonFX(ClimbConstants.CLIMB_MOTOR_ID2);
   private TalonFXConfiguration m_climbMotorConfig = new TalonFXConfiguration();
   private TalonFXConfiguration m_followMotorConfig = new TalonFXConfiguration();
+  private Slot0Configs m_PIDConfig = new Slot0Configs();
 
   private final CurrentLimitsConfigs m_currentLimitConfig = new CurrentLimitsConfigs();
 
@@ -80,13 +77,30 @@ public class Climb extends SubsystemBase {
   }
 
   private void configureMotors() {
-    m_currentLimitConfig.withSupplyCurrentLowerLimit(ClimbConstants.LOCK_CURRENT_LIMIT);
+    m_currentLimitConfig.withSupplyCurrentLimit(ClimbConstants.CLIMB_CURRENT_LIMIT)
+        .withSupplyCurrentLimitEnable(true)
+        .withStatorCurrentLimit(ClimbConstants.CLIMB_CURRENT_LIMIT)
+        .withStatorCurrentLimitEnable(true);
+
+    m_climbMotorConfig.CurrentLimits = m_currentLimitConfig;
+    m_climbMotor.setNeutralMode(NeutralModeValue.Brake);
+    m_climbMotor.getConfigurator().apply(m_climbMotorConfig);
+
+    m_followMotor.getConfigurator().apply(m_followMotorConfig);
 
     m_lockClimbMotorConfig.smartCurrentLimit(ClimbConstants.LOCK_CURRENT_LIMIT);
     m_lockClimbMotorConfig.idleMode(IdleMode.kCoast);
 
     m_lockClimbMotor.configure(m_lockClimbMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
+  }
+
+  private void configurePID() {
+    m_PIDConfig.kP = 1.0;
+    m_PIDConfig.kI = 0.0;
+    m_PIDConfig.kD = 0.0;
+    // subject to change
+    m_climbMotor.getConfigurator().apply(m_PIDConfig);
   }
 
   private void lockClimb() {
