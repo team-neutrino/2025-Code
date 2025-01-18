bc4fabdf60de3ab19dbe2178442db4e4e31ba258
Neutrino Angus <neutrino3928@outlook.com>
2025-01-18 10:34:23 -0600

    Made arm individual for every command

--- 2025-Code/src/main/java/frc/robot/command_factories/ArmFactory.java
+++ 2025-Code/src/main/java/frc/robot/command_factories/ArmFactory.java
@@ -6,25 +6,28 @@ import frc.robot.util.Subsystem;
 import frc.robot.subsystems.Arm;
 
 public class ArmFactory {
-    private static Arm arm = Subsystem.arm;
-
     public static Command moveToL1() {
+        Arm arm = Subsystem.arm;
         return arm.ArmRotateCommand(ArmConstants.L1_POSITION);
     }
 
     public static Command moveToL2() {
+        Arm arm = Subsystem.arm;
         return arm.ArmRotateCommand(ArmConstants.L2_POSITION);
     }
 
     public static Command moveToL3() {
+        Arm arm = Subsystem.arm;
         return arm.ArmRotateCommand(ArmConstants.L3_POSITION);
     }
 
     public static Command moveToL4() {
+        Arm arm = Subsystem.arm;
         return arm.ArmRotateCommand(ArmConstants.L4_POSITION);
     }
 
     public static Command armToIntake() {
+        Arm arm = Subsystem.arm;
         return arm.ArmRotateCommand(ArmConstants.CORAL_STATION_POSITION);
     }
     public static Command armToIntake() {
@@ -32,11 +35,13 @@ public class ArmFactory {
     }
 
     public static Command armToDescoreL2() {
-        return arm.ArmRotateCommand(REEF_L2_DESCORE_POSITION);
+        Arm arm = Subsystem.arm;
+        return arm.ArmRotateCommand(ArmConstants.REEF_L2_DESCORE_POSITION);
     }
 
     public static Command armToDescoreL3() {
-        return arm.ArmRotateCommand(REEF_L3_DESCORE_POSITION);
+        Arm arm = Subsystem.arm;
+        return arm.ArmRotateCommand(ArmConstants.REEF_L3_DESCORE_POSITION);
     }
 
     public static Command armToScore(double level) {
