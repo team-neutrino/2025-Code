d77d8ff380d0d54681d7c25ef7b70776a75d823a
Neutrino Beefmaster <neutrino3928@outlook.com>
2025-01-17 18:30:25 -0600

    did an oopsie

--- 2025-Code/src/main/java/frc/robot/command_factories/ClawFactory.java
+++ 2025-Code/src/main/java/frc/robot/command_factories/ClawFactory.java
@@ -3,9 +3,10 @@ package frc.robot.command_factories;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.ClawConstants;
 import frc.robot.subsystems.Claw;
+import frc.robot.util.Subsystem;
 
 public class ClawFactory {
-    private static Claw claw = new Claw();
+    private static Claw claw = Subsystem.claw;
 
     public static Command wristToIntake() {
         return claw.rotateWrist(ClawConstants.WRIST_INTAKE_POS);
