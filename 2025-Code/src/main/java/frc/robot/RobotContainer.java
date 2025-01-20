795c05c93f5abb94f7dc08b8f36b4ae4ab460952
Nathan Poag <886730poa@ames.k12.ia.us>
2025-01-19 20:50:35 -0600

    initial stuff

--- 2025-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2025-Code/src/main/java/frc/robot/RobotContainer.java
@@ -41,6 +41,8 @@ public class RobotContainer {
     m_driverController.b().whileTrue(ElevatorFactory.moveL3());
     m_driverController.a().whileTrue(ElevatorFactory.moveL4());
 
+    m_driverController.leftBumper().whileTrue(SuperstructureFactory.autoAlign(m_driverController));
+
     m_driverController.leftBumper().whileTrue(ArmFactory.moveToL1());
     m_driverController.back().whileTrue(swerve.resetYawCommand());
     m_driverController.start().onTrue(new InstantCommand(() -> limelight.updateOdometry()));
--- 2025-Code/src/main/java/frc/robot/util/Subsystem.java
+++ 2025-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -7,7 +7,7 @@ public class Subsystem {
     public static final Claw claw = new ClawNT();
     public static final Wrist wrist = new WristNT();
     public static final Elevator elevator = new ElevatorNT();
-    public static final Swerve swerve = null; // new Swerve();
+    public static final Swerve swerve = new Swerve();
     public static final Climb climb = new Climb();
     public static final Limelight limelight = new Limelight();
     public static final LED LED = new LED();
