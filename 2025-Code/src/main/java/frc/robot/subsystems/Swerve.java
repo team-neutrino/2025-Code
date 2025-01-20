20dc36db373a8e9584cd66e0baa5c7ed67cdf6f9
Nathan Poag <886730poa@ames.k12.ia.us>
2025-01-19 20:50:35 -0600

    initial stuff

--- 2025-Code/src/main/java/frc/robot/subsystems/Swerve.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Swerve.java
@@ -13,6 +13,7 @@ import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
