4eaeab9d237fd909a8bdf12255e35eb19f890bf0
Andrew Gurik <gurellia53@gmail.com>
2025-01-21 21:38:28 -0600

    create elevator test but it crashes due to not releasing hardware

---
+++ 2025-Code/src/test/java/frc/robot/subsystem/ElevatorTest.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystem;
+
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.hal.HAL;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.NetworkTables.ElevatorNT;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ElevatorTest {
+
+    Elevator elevatorSubsystem;
+
+    @BeforeEach
+    void setup() {
+        assert HAL.initialize(500, 0);
+    }
+
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
+    @AfterEach
+    void shutdown() throws Exception {
+    }
+
+    @Test
+    void testElevatorConstructionAndPositionGetters() {
+        elevatorSubsystem = new Elevator();
+        var target = elevatorSubsystem.getTargetPosition();
+        var actual = elevatorSubsystem.getEncoderPosition();
+        System.out.println("elevator target and measured " + target + " " + actual);
+    }
+
+    @Test
+    void testElevatorNTConstructionAndPositionGetters() {
+        elevatorSubsystem = new ElevatorNT();
+        var target = elevatorSubsystem.getTargetPosition();
+        var actual = elevatorSubsystem.getEncoderPosition();
+        System.out.println("elevatorNT target and measured " + target + " " + actual);
+    }
+}
