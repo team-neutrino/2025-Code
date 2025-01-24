67ff8350d49992eccf07e24430483a09e03e0d8c
Neutrino Palkia <neutrino3928@outlook.com>
2025-01-23 19:45:22 -0600

    Create ClawNT

---
+++ 2025-Code/src/main/java/frc/robot/subsystems/NetworkTables/ClawNT.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.NetworkTables;
+
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleTopic;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+
+import frc.robot.subsystems.Claw;
+
+public class ClawNT extends Claw {
+    NetworkTableInstance nt = NetworkTableInstance.getDefault();
+    DoubleTopic encoderVelocity = nt.getDoubleTopic("/claw/encoder_velocity");
+    DoubleTopic voltage = nt.getDoubleTopic("/claw/motor_input_voltage");
+    final DoublePublisher encoderVelocityPub;
+    final DoublePublisher motorVoltagePub;
+
+    public ClawNT() {
+        encoderVelocityPub = encoderVelocity.publish();
+        encoderVelocityPub.setDefault(0.0);
+
+        motorVoltagePub = voltage.publish();
+        motorVoltagePub.setDefault(0.0);
+    }
+
+    @Override
+    public void periodic() {
+        super.periodic();
+        final long now = NetworkTablesJNI.now();
+        encoderVelocityPub.set(getVelocityOfGrabber(), now);
+        motorVoltagePub.set(getIntakeVoltage(), now);
+    }
+}
