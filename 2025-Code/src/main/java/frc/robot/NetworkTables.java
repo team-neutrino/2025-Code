1bd1ff656080ecb2b611865dcfc5b82be4853953
Neutrino Beefmaster <neutrino3928@outlook.com>
2025-01-25 15:38:54 -0600

    pdh.java and networkTables.java for battery voltage

---
+++ 2025-Code/src/main/java/frc/robot/NetworkTables.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleTopic;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+import frc.robot.util.PDH;
+
+public class NetworkTables {
+    PDH m_Pdh = new PDH();
+
+    NetworkTableInstance nt = NetworkTableInstance.getDefault();
+
+    DoubleTopic total_current = nt.getDoubleTopic("/PDH/total_current");
+    final DoublePublisher totalCurrent;
+
+    public NetworkTables() {
+        totalCurrent = total_current.publish();
+        totalCurrent.setDefault(0.0);
+    }
+
+    public void periodic() {
+        final long now = NetworkTablesJNI.now();
+        totalCurrent.set(m_Pdh.getTotalCurrent(), now);
+    }
+
+}
