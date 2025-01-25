fb1c966ce09d50a11e04d7a1fe9bd0a7ec2e9ae4
Neutrino Angus <neutrino3928@outlook.com>
2025-01-25 14:59:17 -0600

    fixed autoalign again
    :(

--- 2025-Code/src/main/java/frc/robot/Constants.java
+++ 2025-Code/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
  * numerical or boolean
