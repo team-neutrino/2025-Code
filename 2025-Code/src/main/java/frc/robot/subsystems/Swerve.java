642a0033352a9cadabfa99c38bfab387b76becc0
Neutrino Angus <neutrino3928@outlook.com>
2025-01-25 12:11:48 -0600

    fixed autoalign

--- 2025-Code/src/main/java/frc/robot/subsystems/Swerve.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Swerve.java
@@ -21,6 +21,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import static edu.wpi.first.units.Units.Amps;
