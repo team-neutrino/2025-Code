5c7e1404fc1f0964e3c560bf022e91709c7bfd89
Nathan Poag <886730poa@ames.k12.ia.us>
2025-01-27 17:07:46 -0600

    all the math is implemented!!
    ready for testing, afterwards implement range at which driver control is overridden and driver control override

--- 2025-Code/src/main/java/frc/robot/commands/DriveAssistCom.java
+++ 2025-Code/src/main/java/frc/robot/commands/DriveAssistCom.java
@@ -6,7 +6,6 @@ package frc.robot.commands;
 
 import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentricFacingAngle;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
