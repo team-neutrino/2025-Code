d45c998cf13f4ac784a679c56838545088c9d3d9
Adam Zhu <887022zhu@ames.k12.ia.us>
2025-02-03 19:39:05 -0600

    Updated and finalized climb subsystem based on position instead of volt

--- 2025-Code/src/main/java/frc/robot/command_factories/ClimbFactory.java
+++ 2025-Code/src/main/java/frc/robot/command_factories/ClimbFactory.java
@@ -1,22 +1,21 @@
 package frc.robot.command_factories;
 
 import edu.wpi.first.wpilibj2.command.Command;
+
+import static frc.robot.Constants.ClimbConstants.CLIMB_DOWN_POSITION;
+import static frc.robot.Constants.ClimbConstants.CLIMB_UP_POSITION;
 import static frc.robot.util.Subsystem.climb;
 
 public class ClimbFactory {
     public static Command raiseClimb() {
-        return climb.raiseClimbArmCommand();
+        return climb.moveClimbArmCommand(CLIMB_UP_POSITION);
     }
 
     public static Command lowerClimb() {
-        return climb.lowerClimbArmCommand();
+        return climb.moveClimbArmCommand(CLIMB_DOWN_POSITION);
     }
 
     public static Command lockGrabber() {
         return climb.lockCommand();
-    }
-
-    public static Command test() {
-        return climb.moveToPositionCommand(2);
     }
 }
