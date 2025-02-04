b3b121f50b506ddcd082f221013e49f88b5ca3e3
Adam Zhu <887022zhu@ames.k12.ia.us>
2025-02-03 18:30:04 -0600

    changes that allow me to test position control. Help me

--- 2025-Code/src/main/java/frc/robot/subsystems/Climb.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/Climb.java
@@ -4,9 +4,10 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -14,9 +15,11 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import static frc.robot.Constants.ClimbConstants.*;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -41,6 +44,7 @@ public class Climb extends SubsystemBase {
 
   public Climb() {
     configureMotors();
+    resetEncoderPosition();
   }
 
   private void configureMotors() {
@@ -61,6 +65,29 @@ public class Climb extends SubsystemBase {
 
     m_lockMotor.configure(m_lockMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
+
+    private void resetEncoderPosition() {
+        m_climbMotor.setPosition(0);
+    }
+    
+    public Command moveToPositionCommand(double targetRotations) {
+        return new RunCommand(() -> {
+            PositionVoltage positionControl = new PositionVoltage(targetRotations);
+            m_climbMotor.setControl(positionControl);
+        }, this);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
   public void climbUp() {
     m_climbMotor.setVoltage(CLIMB_UP_VOLTAGE);
