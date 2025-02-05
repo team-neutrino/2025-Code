d02a2e8389fa3e44abb38f78cdd0c051a8052ab5
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-02-04 18:04:35 -0600

    cleaned up constants import statements.

--- 2025-Code/src/main/java/frc/robot/command_factories/ClimbFactory.java
+++ 2025-Code/src/main/java/frc/robot/command_factories/ClimbFactory.java
@@ -2,8 +2,7 @@ package frc.robot.command_factories;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-import static frc.robot.Constants.ClimbConstants.CLIMB_DOWN_POSITION;
-import static frc.robot.Constants.ClimbConstants.CLIMB_UP_POSITION;
+import static frc.robot.Constants.ClimbConstants.*;
 import static frc.robot.util.Subsystem.climb;
 
 public class ClimbFactory {
