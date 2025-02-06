4e0abe911d62acf15b4581f29a2fb362ce694246
Neutrino Highland <neutrino3928@outlook.com>
2025-02-05 19:34:48 -0600

    f

--- 2025-Code/src/main/java/frc/robot/util/Subsystem.java
+++ 2025-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -12,7 +12,7 @@ public class Subsystem {
     public static final Claw claw = new ClawNT();
     public static final Wrist wrist = new WristNT();
     public static final Elevator elevator = new ElevatorNT();
-    public static Swerve swerve = null;
+    public static Swerve swerve = new Swerve();
     public static final Climb climb = new Climb();
     public static final Limelight limelight = new Limelight();
     public static final LED LED = new LED();
