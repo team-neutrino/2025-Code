d15a78f4c21d31ff2efed5d4d2e10146f0340ba8
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-02-12 16:54:37 -0600

    match time on dashboard

--- 2025-Code/src/main/java/frc/robot/Dashboard.java
+++ 2025-Code/src/main/java/frc/robot/Dashboard.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -9,10 +11,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Dashboard extends SubsystemBase {
     private ShuffleboardTab m_driverstationTab;
 
-    // nt for match time
-    private NetworkTables nt = new NetworkTables();
+    // nt
+    // private NetworkTables nt = new NetworkTables();
 
-    private NetworkTableEntry m_matchTimeDisplay;
+    private GenericEntry m_info[] = new GenericEntry[1];
 
     public Dashboard() {
 
@@ -24,15 +26,13 @@ public class Dashboard extends SubsystemBase {
     public void driverstationTab() {
         m_driverstationTab = Shuffleboard.getTab("Driverstation Tab");
 
-        m_driverstationTab.add("eg", 0)
-                .withPosition(2, 0)
+        m_info[0] = m_driverstationTab.add("Match Time", 0)
+                .withPosition(4, 0)
                 .withSize(1, 1).getEntry();
-
-        m_matchTimeDisplay = m_driverstationTab.add("Match Time", 0).withPosition(4, 0).withSize(1, 1).getEntry();
     }
 
     @Override
     public void periodic() {
-        m_matchTimeDisplay.setDouble(nt.match_time)
+        m_info[0].setDouble(DriverStation.getMatchTime());
     }
 }
