f4acee5df53f850087d7fd6fa562d39d9d847fee
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-02-13 17:49:36 -0600

    added game piece

--- 2025-Code/src/main/java/frc/robot/Dashboard.java
+++ 2025-Code/src/main/java/frc/robot/Dashboard.java
@@ -12,13 +12,12 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Limelight;
+import frc.robot.util.Subsystem;
 
 public class Dashboard extends SubsystemBase {
     private ShuffleboardTab m_driverstationTab;
 
-    // private NetworkTables nt = new NetworkTables();
-
-    private GenericEntry m_info[] = new GenericEntry[1];
+    private GenericEntry m_info[] = new GenericEntry[2];
     private GenericEntry m_limelightVariables[] = new GenericEntry[2];
 
     private Limelight m_limelight;
@@ -34,18 +33,25 @@ public class Dashboard extends SubsystemBase {
 
         m_info[0] = m_driverstationTab
                 .add("Match Time", 0)
-                .withPosition(2, 0)
+                .withPosition(0, 0)
                 .withSize(6, 4)
                 .withWidget(BuiltInWidgets.kDial)
                 .withProperties(Map.of("min", 0, "max", 150))
                 .getEntry();
 
-        LLFeed = new HttpCamera("limelight1", "http://limelight.local",
+        m_info[1] = m_driverstationTab
+                .add("Game Piece", "No Piece")
+                .withPosition(2, 2)
+                .withSize(2, 2)
+                .withWidget(BuiltInWidgets.kBooleanBox)
+                .getEntry();
+
+        LLFeed = new HttpCamera("limelight1", "http://limelight.local:5800",
                 HttpCameraKind.kMJPGStreamer);
         CameraServer.startAutomaticCapture(LLFeed);
         m_driverstationTab
                 .add(LLFeed)
-                .withPosition(8, 0)
+                .withPosition(6, 0)
                 .withSize(4, 3)
                 .withWidget(BuiltInWidgets.kCameraStream);
     }
@@ -53,5 +59,6 @@ public class Dashboard extends SubsystemBase {
     @Override
     public void periodic() {
         m_info[0].setDouble(DriverStation.getMatchTime());
+        m_info[1].setBoolean(Subsystem.claw.hasGamePiece());
     }
 }
