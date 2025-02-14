22a08dd55438cf6749a378f25e64041363fa67c7
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-02-14 17:40:13 -0600

    ok

---
+++ 2025-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -0,0 +1,23 @@
+package frc.robot.util;
+
+import frc.robot.Dashboard;
+import frc.robot.NetworkTables;
+import frc.robot.subsystems.*;
+import frc.robot.subsystems.NetworkTables.*;
+
+public class Subsystem {
+    public Subsystem(boolean is_valkyrie) {
+        swerve = new Swerve(is_valkyrie);
+        limelight = new Limelight();
+    }
+
+    public static final Claw claw = new ClawNT();
+    public static final Elevator elevator = new ElevatorNT();
+    public static final Climb climb = new ClimbNT();
+    public static Swerve swerve = null;
+    public static Limelight limelight = null;
+    public static final LED LED = new LED();
+    public static final Arm arm = new ArmNT();
+    public static final NetworkTables networkTables = new NetworkTables();
+    public static final Dashboard dashboard = new Dashboard();
+}
