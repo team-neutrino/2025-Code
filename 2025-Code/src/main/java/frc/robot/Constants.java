f4795fa3cf39624347e3f5cbf71825c2c8e50a1e
Neutrino Angus <neutrino3928@outlook.com>
2025-02-15 16:34:35 -0600

    new limelight height

--- 2025-Code/src/main/java/frc/robot/Constants.java
+++ 2025-Code/src/main/java/frc/robot/Constants.java
@@ -177,7 +177,7 @@ public final class Constants {
     // placeholder values (in meters)
     public static final double CAMERA_FORWARD_OFFSET = 0.216;
     public static final double CAMERA_SIDE_OFFSET = 0.121;
-    public static final double CAMERA_HEIGHT_OFFSET = 0.552;
+    public static final double CAMERA_HEIGHT_OFFSET = 0.342;
     // placeholder values (in degrees)
     public static final double CAMERA_ROLL_OFFSET = 0.0;
     public static final double CAMERA_PITCH_OFFSET = 0.0;
