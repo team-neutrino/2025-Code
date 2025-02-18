60cfb0533c7373b3e6a1babde060b14ff40d33d1
Neutrino Palkia <neutrino3928@outlook.com>
2025-02-17 19:26:10 -0600

    working????

--- 2025-Code/src/main/java/frc/robot/commands/DriveAssistCom.java
+++ 2025-Code/src/main/java/frc/robot/commands/DriveAssistCom.java
@@ -117,7 +117,10 @@ public class DriveAssistCom extends Command {
     Translation2d finalVelocities = null;
     double inputX = m_controller.getLeftY();
     double inputY = m_controller.getLeftX();
-    double inputAngle = Math.toDegrees(Math.atan(-inputX / inputY));
+    double inputAngle = Math.toDegrees(Math.atan2(inputX, inputY));
+    if (inputAngle < 0) {
+      inputAngle += 360;
+    }
     // if (inputAngle < 0) {
     // inputAngle += -45;
     // } else {
@@ -157,18 +160,10 @@ public class DriveAssistCom extends Command {
           break;
       }
       magnitude = Math.sqrt(Math.pow(inputX, 2) + Math.pow(inputY, 2));
-      // if (inputAngle < 30) {
-      // // input is right of april tag (facing it)
-      // otherAngle = 30 - Math.abs(inputAngle);
-      // } else {
-      // otherAngle = inputAngle - 30;
-      // }
-      otherAngle = inputAngle;
-      desiredMagnitude = Math.sin(Math.toRadians(otherAngle)) * magnitude;
+      desiredMagnitude = Math.cos(Math.toRadians(inputAngle - quadrantOffset)) * magnitude;
       desiredX = desiredMagnitude * (Math.cos(Math.toRadians(quadrantOffset)));
       desiredY = desiredMagnitude * (Math.sin(Math.toRadians(quadrantOffset)));
       finalVelocities = new Translation2d(desiredX, desiredY);
-      System.out.println(inputAngle);
     }
     if (finalVelocities == null) {
       return new Translation2d(0, 0);
