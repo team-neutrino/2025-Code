d8bf860c6476bb1bb57cecfd9d191e01c9c41bb8
Neutrino Angus <neutrino3928@outlook.com>
2025-02-22 15:14:25 -0600

    fix

--- 2025-Code/src/main/java/frc/robot/util/DriveToPointCalculator.java
+++ 2025-Code/src/main/java/frc/robot/util/DriveToPointCalculator.java
@@ -5,32 +5,32 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class DriveToPointCalculator {
-    public static Pose2d CalculatePoint(Pose3d tagPosition, double offsetFromReef, double offsetOfArm,
-            double reefWidth, boolean isA) {
-        double leftRightOffsetX;
-        double leftRightOffsetY;
-        double reefAngle = tagPosition.getRotation().getAngle();
-        double perpendicularReefAngle = reefAngle - Math.toRadians(90);
-        if (isA) {
-            leftRightOffsetX = offsetFromReef * Math.cos(perpendicularReefAngle)
-                    + (reefWidth / 2) * Math.cos(perpendicularReefAngle);
-            leftRightOffsetY = offsetFromReef * Math.sin(perpendicularReefAngle)
-                    + (reefWidth / 2) * Math.sin(perpendicularReefAngle);
+        public static Pose2d CalculatePoint(Pose3d tagPosition, double offsetFromReef, double offsetOfArm,
+                        double reefWidth, boolean isA) {
+                double leftRightOffsetX;
+                double leftRightOffsetY;
+                double reefAngle = tagPosition.getRotation().getAngle();
+                double perpendicularReefAngle = reefAngle - Math.toRadians(90);
+                if (isA) {
+                        leftRightOffsetX = offsetFromReef * Math.cos(perpendicularReefAngle)
+                                        + (reefWidth / 2) * Math.cos(perpendicularReefAngle);
+                        leftRightOffsetY = offsetFromReef * Math.sin(perpendicularReefAngle)
+                                        + (reefWidth / 2) * Math.sin(perpendicularReefAngle);
 
-        } else {
-            leftRightOffsetX = offsetFromReef * Math.cos(perpendicularReefAngle)
-                    + (reefWidth / 2) * Math.cos(perpendicularReefAngle + Math.PI);
-            leftRightOffsetY = offsetFromReef * Math.sin(perpendicularReefAngle)
-                    + (reefWidth / 2) * Math.sin(perpendicularReefAngle + Math.PI);
+                } else {
+                        leftRightOffsetX = offsetFromReef * Math.cos(perpendicularReefAngle)
+                                        + (reefWidth / 2) * Math.cos(perpendicularReefAngle + Math.PI);
+                        leftRightOffsetY = offsetFromReef * Math.sin(perpendicularReefAngle)
+                                        + (reefWidth / 2) * Math.sin(perpendicularReefAngle + Math.PI);
+
+                }
+
+                double x = tagPosition.getX() + offsetFromReef * Math.cos(reefAngle)
+                                + leftRightOffsetX;
+                double y = tagPosition.getY() + offsetFromReef * Math.cos(reefAngle)
+                                + leftRightOffsetY;
+
+                return new Pose2d(x, y, Rotation2d.fromRadians(reefAngle + Math.PI));
 
         }
-
-        double x = tagPosition.getX() + offsetFromReef * Math.cos(reefAngle)
-                + leftRightOffsetX;
-        double y = tagPosition.getY() + offsetFromReef * Math.cos(reefAngle)
-                + leftRightOffsetY;
-
-        return new Pose2d(x, y, Rotation2d.fromDegrees(reefAngle + Math.toRadians(180)));
-
-    }
 }
