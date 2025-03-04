06cfc137766ced1b6830ff12a304acc80088231c
Neutrino - Florida Cracker <neutrino3928@outlook.com>
2025-03-03 19:44:09 -0600

    might be done, need to clean up

--- 2025-Code/src/main/java/frc/robot/subsystems/LED.java
+++ 2025-Code/src/main/java/frc/robot/subsystems/LED.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.networktables.StringTopic;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StringPublisher;
 import frc.robot.util.Subsystem;
@@ -31,12 +30,15 @@ public class LED extends SubsystemBase {
     if(Subsystem.swerve.isDrivingToPoint() && !Subsystem.elevator.atTargetHeight() && !Subsystem.arm.atTargetAngle()){
       m_statepub.set("solid");
       m_colorpub.set("cyan");
+      return;
     } else if (Subsystem.swerve.isDrivingToPoint() && !Subsystem.elevator.readyToScore()) {
       m_statepub.set("solid");
       m_colorpub.set("purple");
+      return;
     }else if (Subsystem.swerve.isDrivingToPoint()) {
       m_statepub.set("solid");
       m_colorpub.set("red");
+      return;
     }
 
     if (Subsystem.swerve.isAtPoint() && Subsystem.elevator.readyToScore() && Subsystem.arm.readyToScore()) {
