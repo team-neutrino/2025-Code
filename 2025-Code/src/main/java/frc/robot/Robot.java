4902a3e7ff3db50055259b37e4fca4ced47e637e
Neutrino Angus <neutrino3928@outlook.com>
2025-03-11 16:41:44 -0500

    delete unused import

--- 2025-Code/src/main/java/frc/robot/Robot.java
+++ 2025-Code/src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 import java.util.Optional;
 
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
