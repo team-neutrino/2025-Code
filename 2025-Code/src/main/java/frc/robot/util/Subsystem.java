91a31915577160a0fd603a7be8b2f82594803d93
Neutrino Angus <neutrino3928@outlook.com>
2025-03-29 10:03:35 -0500

    no nt

--- 2025-Code/src/main/java/frc/robot/util/Subsystem.java
+++ 2025-Code/src/main/java/frc/robot/util/Subsystem.java
@@ -1,7 +1,6 @@
 package frc.robot.util;
 
 import frc.robot.subsystems.*;
-import frc.robot.subsystems.NetworkTables.*;
 
 public class Subsystem {
     public Subsystem(boolean is_valkyrie) {
@@ -10,11 +9,11 @@ public class Subsystem {
     }
 
     public static final Coral coral = new Coral();
-    public static final Elevator elevator = new ElevatorNT();
+    public static final Elevator elevator = new Elevator();
     public static final Climb climb = new Climb();
     public static Swerve swerve = null;
     public static Limelight limelight = null;
-    public static final Arm arm = new ArmNT();
+    public static final Arm arm = new Arm();
     public static final Algae algae = new Algae();
     public static final LED LED = new LED();
 }
