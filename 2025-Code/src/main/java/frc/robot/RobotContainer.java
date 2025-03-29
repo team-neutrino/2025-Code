4c5368c97e3a8b88204cfaf9e3cb86e047ff28b5
Neutrino Angus <neutrino3928@outlook.com>
2025-03-29 09:29:27 -0500

    correct auton

--- 2025-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2025-Code/src/main/java/frc/robot/RobotContainer.java
@@ -44,7 +44,7 @@ public class RobotContainer {
     configureDefaultCommands();
     configureNamedCommands();
     DataLogManager.start();
-    m_autonPath = new PathPlannerAuto("3 CORAL PROCESSOR");
+    m_autonPath = new PathPlannerAuto("3 CORAL TOP");
 
   }
 
