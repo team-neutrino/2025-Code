60e961c906f71e6289591739dbb281e328bf4251
Neutrino Beefmaster <neutrino3928@outlook.com>
2025-04-04 20:46:49 -0500

    workin'-ish

--- 2025-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
+++ 2025-Code/src/main/java/frc/robot/command_factories/SuperstructureFactory.java
@@ -1,7 +1,7 @@
 package frc.robot.command_factories;
 
 import edu.wpi.first.wpilibj2.command.Command;
-
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -241,8 +241,9 @@ public class SuperstructureFactory {
     }
 
     public static Command spicyMoveToScoreL4Command() {
-        return ElevatorFactory.moveL4().andThen(ArmFactory.moveToL4())
-                .onlyIf(() -> DriveToPointController.getTotalDistance() < .3);
+        return Commands.either(ElevatorFactory.moveL4().andThen(ArmFactory.moveToL4()),
+                elevator.elevatorDefaultCommand().alongWith(arm.armDefaultCommand()),
+                () -> DriveToPointController.getTotalDistance() < .3);
     }
 
     public static Command moveToScoreL3Command() {
