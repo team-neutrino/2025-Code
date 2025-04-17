a7a50d6b38692706054024d1591dc9c2b465483a
Neutrino Angus <neutrino3928@outlook.com>
2025-04-17 08:41:33 -0500

    good

--- 2025-Code/src/main/java/frc/robot/Constants.java
+++ 2025-Code/src/main/java/frc/robot/Constants.java
@@ -301,7 +301,7 @@ public final class Constants {
                 public static final double SAFE_OFFSET_TO_STATION = 1.5;
 
                 public static final double OFFSET_ARM_ALGAE = 0.31;
-                public static final double OFFSET_REEF_ALGAE = 0.51;
+                public static final double OFFSET_REEF_ALGAE = 0.49;
                 public static final double DYNAMIC_INTAKE_THRESHOLD = .5;
                 public static final double OFFSET_TO_BARGE_X = .57;
                 public static final double OFFSET_TO_BARGE_Y = 1.2;
